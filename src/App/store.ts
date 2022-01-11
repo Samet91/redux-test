72d15bd63c7f5a648663caa95627d3caa5de2f08
Samet Uslu <samet_u@hotmail.de>
2022-01-11 18:00:28 +0100

    create ratingreducer

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookmarkreducer from "./slices/bookmarks/BookmarksSlice";
 import { BooksApiSlice } from "./slices/booksApi/BooksApiSlice";
+import ratingreducer from "./slices/Rating/Rating.slice";
 
 export const store = configureStore({
   reducer: {
     [BooksApiSlice.reducerPath]: BooksApiSlice.reducer,
     bookmarks: bookmarkreducer,
+    ratings: ratingreducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(BooksApiSlice.middleware);
