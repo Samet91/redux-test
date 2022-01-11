5b34d1f5143c763d946ab5f0ac9c9963cb6afc81
Samet Uslu <samet_u@hotmail.de>
2022-01-11 17:19:54 +0100

    RatingSlice finish

--- src/App/slices/Rating/Rating.slice.ts
+++ src/App/slices/Rating/Rating.slice.ts
@@ -1,9 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState: number[] = [];
+export interface ratingObject {
+  id: number;
+  rating: number;
+}
+
+const initialState: ratingObject[] = [];
 
 export const RatingSlice = createSlice({
   name: "rating",
   initialState,
-  reducers: {},
+  reducers: {
+    setRating(state = initialState, action) {
+      return (state = [...state, action.payload]); // bei react ohne den ersten state
+    },
+  },
 });
+
+export const { setRating } = RatingSlice.actions;
+export default RatingSlice.reducer;
