6f1774c07e0e1573bd8971c0c364d16b338b59ee
Samet Uslu <samet_u@hotmail.de>
2022-01-11 15:39:36 +0100

    integrate heart icon

--- src/components/Card/card.tsx
+++ src/components/Card/card.tsx
@@ -1,18 +1,29 @@
 import classes from "./card.module.css";
-import {} from "react-icons/ai";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 type cardProps = {
   title: string;
   author: string;
   image: string;
+  bookmarked: boolean;
+  handleClick: () => void;
 };
 
-export default function Card({ title, author, image }: cardProps): JSX.Element {
+export default function Card({
+  title,
+  author,
+  image,
+  bookmarked,
+  handleClick,
+}: cardProps): JSX.Element {
   return (
     <div className={classes.card_area_div}>
       <h2>Title: {title}</h2>
       <p>Author: {author}</p>
       <img src={image} height="200" />
+      <div onClick={handleClick}>
+        {bookmarked ? <AiFillHeart /> : <AiOutlineHeart />}
+      </div>
     </div>
   );
 }
